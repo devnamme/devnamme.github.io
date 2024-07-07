cc245296b92df679dc336d9afe3f60786eb4cb8a
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-07 16:09:32 +0800

    cleanup

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,43 +1,14 @@
 import { useEffect, useRef } from "react";
 import { WorksData, WorksDataGroups } from "../../data";
 import "./index.css";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface Props {
   setSlug: Function;
 }
 
 function Gallery({ setSlug }: Props) {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
   const galleryRef = useRef(null);
-  const refs = useRef<(HTMLElement | null)[]>([null, null, null]);
-
-  const paths = ["/works/web", "/works/mobile", "/works/game"];
-
-  // const onScroll = () => {
-  //   for (let i = 0; i < refs.current.length; i++) {
-  //     if (refs.current[i] == null) continue;
-
-  //     let bcr = refs.current[i].getBoundingClientRect();
-  //     let left = bcr.left;
-
-  //     let right;
-  //     right = galleryRef.current.getBoundingClientRect().right;
-  //     // if (i < 2) right = refs.current[i + 1].getBoundingClientRect().left;
-  //     // else right = galleryRef.current.getBoundingClientRect().right;
-
-  //     if (left <= window.innerWidth / 2) {
-  //       // if (pathname != paths[i]) navigate(paths[i], { replace: true });
-  //     }
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   window.removeEventListener("scroll", onScroll);
-  //   window.addEventListener("scroll", onScroll, { passive: true });
-  //   return () => window.removeEventListener("scroll", onScroll);
-  // }, []);
 
   return (
     <div className="gallery" ref={galleryRef}>
